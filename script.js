6ea5b7c1f75a0c4b9f7705a8cd9b55f05fe5e01f
ctrlgear <smolenschi.mihai@gmail.com>
2022-07-07 09:32:54 +0300

    implemented computerPlay

---
+++ script.js
@@ -0,0 +1,14 @@
+function computerChooses() {
+  let result = "";
+
+  // 0 - rock, 1 - paper, 2 - scrissors
+  const rand = Math.floor(Math.random() * 3);
+
+  if (rand === 0) result = "rock";
+  else if (rand === 1) result = "paper";
+  else if (rand === 2) result = "scissors";
+
+  return result;
+}
+
+console.log(computerChooses());
